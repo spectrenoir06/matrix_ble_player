0f324a2afc1fcdfe0f5767f2fab11f3d232b7fe2
Antoine Doussaud <antoine@doussaud.org>
2024-02-11 17:02:26 +0100

    hue shift work + optimise render

--- src/Gif.cpp
+++ src/Gif.cpp
@@ -14,6 +14,86 @@
 #define filesystem SPIFFS
 #endif
 
+uint16_t HHH = 0;
+
+void rgb2hsv(const uint8_t src_r, const uint8_t src_g, const uint8_t src_b, uint16_t *dst_h, uint8_t *dst_s, uint8_t *dst_v){
+    float r = src_r / 255.0f;
+    float g = src_g / 255.0f;
+    float b = src_b / 255.0f;
+
+    float h, s, v; // h:0-360.0, s:0.0-1.0, v:0.0-1.0
+
+    float M = max(r, max(g, b));
+    float m = min(r, min(g, b));
+
+    v = M;
+
+    if (M == 0.0f) {
+        s = 0;
+        h = 0;
+    }
+    else if (M - m == 0.0f) {
+        s = 0;
+        h = 0;
+    }
+    else {
+        s = (M - m) / M;
+
+        if (M == r) {
+            h = 60 * ((g - b) / (M - m)) + 0;
+        }
+        else if (M == g) {
+            h = 60 * ((b - r) / (M - m)) + 120;
+        }
+        else {
+            h = 60 * ((r - g) / (M - m)) + 240;
+        }
+    }
+
+    if (h < 0) h += 360.0f;
+
+    *dst_h = (uint16_t)(h);   // dst_h : 0-180
+    *dst_s = (unsigned char)(s * 255); // dst_s : 0-255
+    *dst_v = (unsigned char)(v * 255); // dst_v : 0-255
+}
+
+void hsv2rgb(const uint16_t src_h, const uint8_t src_s, const uint8_t src_v, uint8_t *dst_r, uint8_t *dst_g, uint8_t *dst_b) {
+    // src_h = src_h % 360;
+    float h = src_h%360; // 0-360
+    float s = src_s / 255.0f; // 0.0-1.0
+    float v = src_v / 255.0f; // 0.0-1.0
+
+    float r, g, b; // 0.0-1.0
+
+    int   hi = (int)(h / 60.0f) % 6;
+    float f  = (h / 60.0f) - hi;
+    float p  = v * (1.0f - s);
+    float q  = v * (1.0f - s * f);
+    float t  = v * (1.0f - s * (1.0f - f));
+
+    switch(hi) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+
+    *dst_r = (uint8_t)(r * 255); // dst_r : 0-255
+    *dst_g = (uint8_t)(g * 255); // dst_r : 0-255
+    *dst_b = (uint8_t)(b * 255); // dst_r : 0-255
+}
+
+void color_shift(CRGB *col, int32_t shift) {
+    uint8_t s,v;
+    uint16_t h;
+    if ((col->r != col->g) || (col->g != col->b)) {
+      rgb2hsv(col->r, col->g, col->b, &h, &s, &v);
+      hsv2rgb(h + shift, s, v, &col->r, &col->g, &col->b);
+    }
+}
+
 extern VirtualMatrixPanel *virtualDisp;
 extern void flip_matrix();
 uint8_t disposalMethod = 0;
@@ -30,7 +110,7 @@ namespace {
   void GIFDraw(GIFDRAW *pDraw) {
     disposalMethod = pDraw->ucDisposalMethod;
     uint8_t *s;
-    uint16_t *d, *usPalette, usTemp[320];
+    CRGB *usPalette;
     int y, iWidth;
 
     iWidth = pDraw->iWidth;
@@ -40,67 +120,17 @@ namespace {
     int off_x = (V_MATRIX_WIDTH  - gif.getCanvasWidth() )/2;
     int off_y = (V_MATRIX_HEIGHT - gif.getCanvasHeight())/2;
 
-    usPalette = pDraw->pPalette;
+    usPalette = (CRGB*)pDraw->pPalette24;
     y = pDraw->iY + pDraw->y; // current line
-
     s = pDraw->pPixels;
-    // if (pDraw->ucDisposalMethod == 2) { // restore to background color
-    //   // for (int x = 0; x < iWidth; x++) {
-    //   //   if (s[x] == pDraw->ucTransparent)
-    //   //     s[x] = pDraw->ucBackground;
-    //   // }
-    //   pDraw->ucHasTransparency = 0;
-    // }
-    // Apply the new pixels to the main image
-    // if (pDraw->ucHasTransparency) { // if transparency used
-    //   uint8_t *pEnd, c, ucTransparent = pDraw->ucTransparent;
-    //   int x, iCount;
-    //   pEnd = s + pDraw->iWidth;
-    //   x = 0;
-    //   iCount = 0; // count non-transparent pixels
-    //   while (x < pDraw->iWidth) {
-    //     c = ucTransparent - 1;
-    //     d = usTemp;
-    //     while (c != ucTransparent && s < pEnd) {
-    //       c = *s++;
-    //       if (c == ucTransparent) { // done, stop
-    //         s--; // back up to treat it like transparent
-    //       } else { // opaque
-    //         *d++ = usPalette[c];
-    //         iCount++;
-    //       }
-    //     }             // while looking for opaque pixels
-    //     if (iCount) { // any opaque pixels?
-    //       for (int xOffset = 0; xOffset < iCount; xOffset++) {
-    //         virtualDisp->drawPixel(off_x + x + xOffset + pDraw->iX, off_y + y, usTemp[xOffset]); // 565 Color Format
-    //       }
-    //       x += iCount;
-    //       iCount = 0;
-    //     }
-    //     // no, look for a run of transparent pixels
-    //     c = ucTransparent;
-    //     while (c == ucTransparent && s < pEnd) {
-    //       c = *s++;
-    //       if (c == ucTransparent)
-    //         iCount++;
-    //       else
-    //         s--;
-    //     }
-    //     if (iCount) {
-    //       x += iCount; // skip these
-    //       iCount = 0;
-    //     }
-    //   }
-    // }
-    // else { // does not have transparency
-      s = pDraw->pPixels;
-      // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
-      for (int x = 0; x < pDraw->iWidth; x++) {
-        if (!pDraw->ucHasTransparency || *s != pDraw->ucTransparent)
-          virtualDisp->drawPixel(off_x + x + pDraw->iX, off_y + y, usPalette[*s]); // color 565
-        s++;
+    for (int x = 0; x < pDraw->iWidth; x++) {
+      if (!pDraw->ucHasTransparency || *s != pDraw->ucTransparent) {
+          CRGB col = usPalette[*s];
+          // color_shift(&col, HHH + x*5 + y*5);
+          virtualDisp->drawPixel(off_x + x + pDraw->iX, off_y + y, col);
       }
-    // }
+      s++;
+    }
   }
 
   void *GIFOpenFile(const char *fname, int32_t *pSize) {
@@ -142,8 +172,11 @@ namespace {
     return pFile->iPos;
   }
 
+
   void gifTask(void *parameter) {
-    int t, i;
+    int i, t;
+    uint8_t next_frame_ready = 0;
+    uint8_t more_frame = 0;
     for (;;) {
       vTaskDelay(1 / portTICK_PERIOD_MS);
       if (spectre_gif_plz_stop) continue;
@@ -168,27 +201,31 @@ namespace {
           }
         }
       }
-      // next frame
+  
+      if (!next_frame_ready) {
+        more_frame = gif.playFrame(false, &i);
+        next_frame_ready = 1;
+      }
+
       t = millis();
-      if (t < next_frame_millis) continue;
-      // virtualDisp->clearScreen();
-      if (!gif.playFrame(false, &i)) {
-        gif.reset();
-        flip_matrix();
-        virtualDisp->clearScreen();
-      } else {
-        flip_matrix();
-        if (disposalMethod == 2) {
+      if (t >= next_frame_millis) {
+        next_frame_millis = next_frame_millis + i;
+        if (!more_frame) {
+          gif.reset();
+          flip_matrix();
           virtualDisp->clearScreen();
-          // virtualDisp->fillRect(pDraw->iX, pDraw->iY, pDraw->iWidth, pDraw->iHeight, 0);
-          // flip_matrix();
-          // virtualDisp->clearScreen();
         } else {
-          virtualDisp->copyDMABuffer();
+          flip_matrix();
+          if (disposalMethod == 2) {
+            virtualDisp->clearScreen();
+          } else {
+            virtualDisp->copyDMABuffer();
+          }
         }
+        next_frame_ready = 0;
+        HHH += 1;
       }
           // Serial.printf("New frame\n");
-      next_frame_millis = t + i;
 
       // copy front buffer into back buffer
       // virtualDisp->copyDMABuffer();
@@ -209,7 +246,7 @@ namespace SpectreGif {
   }
 
   uint8_t init() {
-    gif.begin(LITTLE_ENDIAN_PIXELS);
+    gif.begin(GIF_PALETTE_RGB888);
     return xTaskCreate(
         gifTask,   /* Task function. */
         "GifTask", /* String with name of task. */
